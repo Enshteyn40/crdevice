584f1ca5bb02bddb16fbde99f96ac607b2a2002a
Asqarov Ro'zimuhammad <68824422+Enshteyn40@users.noreply.github.com>
2024-09-09 04:30:52 +0500

    Update wateranimate.py

--- wateranimate.py
+++ wateranimate.py
@@ -1,1 +1,13 @@
-print("helo world")
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+import csv, os, sys
+from telethon.tl.functions.messages import ImportChatInviteRequest, SendMessageRequest
+from telethon import types, utils, errors
+import random
+from telethon.tl.functions.channels import LeaveChannelRequest
+import time, requests
+with open('phone.csv', 'r') as f:
+    global phlist
+    phlist = [row[0] for row in csv.reader(f)]
+clear_console()
+print('\n\n\n\033[93m\nJami: ' + str(len(phlist)) + " ta Raqamlar mavjud\033[0m\n\n\n")
