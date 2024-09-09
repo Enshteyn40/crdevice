f2dbb50fec7689745884e0bbee54c12035b21942
Asqarov Ro'zimuhammad <68824422+Enshteyn40@users.noreply.github.com>
2024-09-09 05:20:13 +0500

    Update wateranimate.py

--- wateranimate.py
+++ wateranimate.py
@@ -1,13 +1,28 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+import csv
 import os
+import sys
+from telethon.tl.functions.messages import ImportChatInviteRequest, SendMessageRequest
+from telethon import types, utils, errors
+import random
+from telethon.tl.functions.channels import LeaveChannelRequest
+import time
+import requests
 
-# Joriy faylning yo'lini chiqarish
+# Joriy faylning katalogini topamiz
 current_dir = os.path.dirname(os.path.abspath(__file__))
-print("Joriy katalog:", current_dir)
 
-# Bir pog'ona yuqoridagi katalog
+# Bitta yuqoridagi katalogni olamiz
 parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
-print("Yuqoridagi katalog:", parent_dir)
 
-# Fayl yo'lini ko'rsatish
+# Yuqoridagi katalogdagi phone.csv fayliga yo'l yaratamiz
 csv_file_path = os.path.join(parent_dir, 'phone.csv')
-print("Fayl yo'li:", csv_file_path)
+
+# phone.csv faylining mavjudligini tekshiramiz va faylni o'qiymiz
+if os.path.exists(csv_file_path):
+    with open(csv_file_path, 'r') as f:
+        phlist = [row[0] for row in csv.reader(f)]
+        print(f"Topilgan telefonlar soni: {len(phlist)}")
+else:
+    print("phone.csv fayli topilmadi.")
