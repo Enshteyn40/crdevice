328190743524925c0a3b58d7da34d461b16fa205
Asqarov Ro'zimuhammad <68824422+Enshteyn40@users.noreply.github.com>
2024-09-09 05:48:27 +0500

    Update wateranimate.py

--- wateranimate.py
+++ wateranimate.py
@@ -1,12 +1,18 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-import csv, os, sys
-from telethon.tl.functions.messages import ImportChatInviteRequest, SendMessageRequest
-from telethon import types, utils, errors
-import random
-from telethon.tl.functions.channels import LeaveChannelRequest
-import time, requests
-with open('phone.csv', 'r') as f:
-    global phlist
-    phlist = [row[0] for row in csv.reader(f)]
-print('\n\n\n\033[93m\nJami: ' + str(len(phlist)) + " ta Raqamlar mavjud\033[0m\n\n\n")
+import sys
+import csv
+
+# phone.csv faylining to'liq yo'li parametr sifatida uzatiladi
+if len(sys.argv) < 2:
+    print("phone.csv faylining yo'li uzatilmagan.")
+    sys.exit(1)
+
+csv_file_path = sys.argv[1]
+
+# Faylni ochib ma'lumotlarni o'qish
+try:
+    with open(csv_file_path, 'r') as f:
+        phlist = [row[0] for row in csv.reader(f)]
+    print(f"Jami: {len(phlist)} ta raqamlar mavjud")
+except FileNotFoundError:
+    print(f"phone.csv fayli topilmadi: {csv_file_path}")
+    sys.exit(1)
