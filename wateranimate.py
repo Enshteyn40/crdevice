0a1aab0deb14023c67db1938b74e5efabd154251
Asqarov Ro'zimuhammad <68824422+Enshteyn40@users.noreply.github.com>
2024-09-09 05:33:26 +0500

    Update wateranimate.py

--- wateranimate.py
+++ wateranimate.py
@@ -1,30 +1,16 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-import csv
 import os
-import sys
-from telethon.tl.functions.messages import ImportChatInviteRequest, SendMessageRequest
-from telethon import types, utils, errors
-import random
-from telethon.tl.functions.channels import LeaveChannelRequest
-import time
-import requests
 
-# Foydalanuvchidan phone.csv joylashgan katalogga yo'lni so'raymiz
-input_path = input("Iltimos, phone.csv joylashgan katalogga to'liq yo'lni kiriting: ")
+# Joriy skriptning joylashuvini topamiz
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Kirtilgan yo'lning mavjudligini tekshiramiz
-if not os.path.exists(input_path):
-    print(f"Kiritilgan yo'l mavjud emas: {input_path}")
-    sys.exit()
+# Bitta yuqoridagi papkani olamiz
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
 
-# phone.csv fayliga to'liq yo'l yaratamiz
-csv_file_path = os.path.join(input_path, 'phone.csv')
+# phone.csv fayliga yo'l yaratamiz
+csv_file_path = os.path.join(parent_dir, 'phone.csv')
 
-# phone.csv faylining mavjudligini tekshiramiz va faylni o'qiymiz
+# phone.csv faylining mavjudligini tekshiramiz
 if os.path.exists(csv_file_path):
-    with open(csv_file_path, 'r') as f:
-        phlist = [row[0] for row in csv.reader(f)]
-        print(f"Topilgan telefonlar soni: {len(phlist)}")
+    print(f"Fayl topildi: {csv_file_path}")
 else:
-    print(f"phone.csv fayli topilmadi kiritilgan yo'lda: {csv_file_path}")
+    print("phone.csv fayli topilmadi.")
