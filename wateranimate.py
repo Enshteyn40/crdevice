e122d5ca76e3728487df844d354b610573ad07bc
Asqarov Ro'zimuhammad <68824422+Enshteyn40@users.noreply.github.com>
2024-09-09 05:07:03 +0500

    Update wateranimate.py

--- wateranimate.py
+++ wateranimate.py
@@ -6,7 +6,18 @@ from telethon import types, utils, errors
 import random
 from telethon.tl.functions.channels import LeaveChannelRequest
 import time, requests
-with open('phone.csv', 'r') as f:
-    global phlist
-    phlist = [row[0] for row in csv.reader(f)]
-print('\n\n\n\033[93m\nJami: ' + str(len(phlist)) + " ta Raqamlar mavjud\033[0m\n\n\n")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Bitta yuqoridagi katalogni olamiz
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+
+# parent_dir papkadagi phone.csv faylini yuklaymiz
+csv_file_path = os.path.join(parent_dir, 'phone.csv')
+
+# phone.csv faylining mavjudligini tekshiramiz va uni o'qish
+if os.path.exists(csv_file_path):
+    with open(csv_file_path, 'r') as f:
+        phlist = [row[0] for row in csv.reader(f)]
+        print(f"Topilgan telefonlar soni: {len(phlist)}")
+else:
+    print("phone.csv fayli topilmadi.")
